50abca061dcbb77a8e9d053bfc26f7b4d411d83c
zy <xpzouying@gmail.com>
2023-05-14 16:54:49 +0800

    sd: support user input for stable diffusion

--- plugins/stablediffusion/stablediffusion_test.go
+++ plugins/stablediffusion/stablediffusion_test.go
@@ -7,14 +7,15 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestStableDiffusion(t *testing.T) {
 
 	sd := NewStableDiffusion("127.0.0.1:19000")
 
-	answer, err := sd.Do(context.Background(), "a girl")
-	assert.NoError(t, err)
+	answer, err := sd.Do(context.Background(), "a girl play golf")
+	require.NoError(t, err)
 
 	assert.NotEmpty(t, answer)
 
