9ed5e32c325883c303817d37d016dc279cd838ef
zy <xpzouying@gmail.com>
2023-05-14 16:54:49 +0800

    sd: support user input for stable diffusion

--- plugins/stablediffusion/stablediffusion.go
+++ plugins/stablediffusion/stablediffusion.go
@@ -1,6 +1,7 @@
 package stablediffusion
 
 import (
+	"bytes"
 	"context"
 	"encoding/json"
 	"fmt"
@@ -31,10 +32,12 @@ func NewStableDiffusion(sdAddr string) *StableDiffusion {
 }
 
 func (s *StableDiffusion) Do(ctx context.Context, query string) (answer string, err error) {
+	req, err := s.newRequest(ctx, query)
+	if err != nil {
+		return "", err
+	}
 
-	url := fmt.Sprintf("http://%v/sd", s.sdAddr)
-
-	resp, err := s.client.Get(url)
+	resp, err := s.client.Do(req)
 	if err != nil {
 		return "", err
 	}
@@ -54,6 +57,26 @@ func (s *StableDiffusion) Do(ctx context.Context, query string) (answer string, 
 	}
 
 	return sdResp.Images[0], nil
+}
+
+func (s *StableDiffusion) newRequest(ctx context.Context, query string) (*http.Request, error) {
+	url := fmt.Sprintf("http://%v/sd", s.sdAddr)
+
+	body := map[string]interface{}{
+		"text": query,
+	}
+	data, err := json.Marshal(body)
+	if err != nil {
+		return nil, err
+	}
+
+	req, err := http.NewRequest(http.MethodPost, url, bytes.NewReader(data))
+	if err != nil {
+		return nil, err
+	}
+	req = req.WithContext(ctx)
+
+	return req, nil
 }
 
 func (StableDiffusion) GetName() string {
